506d185f9bdfe829faad8ddbec56f3b545df7f82
vinnya3 <vinaceto@yahoo.com>
2016-09-17 16:06:35 -0400

    feat: refactor toString
    Refactored toString method. Shallow cloning array functionality
    was refactored into its own helper method - cloneArray. Provided
    meaningful comments and got rid of redundant ones.

--- life.js
+++ life.js
@@ -1,20 +1,31 @@
 (function() {
 	// Game takes in a seed that serves as the initial state
 	var _  = self.Life = function(seed) {
-		this.seed = seed;
 		this.height = seed.length // Number of rows
 		this.width = seed[0].length // Number of Columns ( Len of sub arr in 2D arr)
+    // boards - prevBoard serves as previous game board
+		this.prevBoard = [];
+		this.board = cloneArray(seed);
 	}
 
 	_.prototype = {
-		//Debugging
 		toString: function() {
-			// return shallow copy of the seed arr and convert to a string
-			return this.seed.slice().map(function(row) {
-				return row.slice().join(' ');
-			}).join('\n'); // separate each row with a new line
+			//Debugging: Convert 2D board arr to string grid representation
+			return this.board.map(function(row) {
+				return row.join(' ');
+			}).join('\n');
 		}
 	}
+
+	// Helpers
+	// ** cloneArray only works on 2D arrays **
+	function cloneArray(arr) {
+		// return shallow copy of the seed arr and convert to a string
+		return arr.slice().map(function(row) {
+			return row.slice();
+		});
+	}
+
 })();
 
 var Game = new Life([
@@ -25,5 +36,4 @@ var Game = new Life([
 	[0,0,0,0,0]
 ]);
 
-// Debug: log out the seed converted to a string depiction of the grid
 console.log(Game.toString());
