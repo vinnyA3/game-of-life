e460dbadb681a9f526de91e89e9f5b1dd78043fc
vinnya3 <vinaceto@yahoo.com>
2016-09-18 02:09:02 -0400

    feat: Add aliveNeighbors algorithm
    Added aliveNeighbors alg that counts the number of on/alive
    neighbors. Added meaningful comments along with debug log

--- life.js
+++ life.js
@@ -9,6 +9,32 @@
 	}
 
 	_.prototype = {
+		next: function() {
+			this.prevBoard = cloneArray(this.board);
+			for(var y = 0; y < this.height; ++y) {
+				for(var x = 0; x < this.width; ++x) {
+					var neighbors = this.aliveNeighbors(this.prevBoard, x, y);
+					// Debug
+					console.log(y, x, ': ', neighbors);
+				}
+			}
+		},
+		aliveNeighbors: function(arr, x, y) {
+			// If the previous or next row does not exist, set to an empty array
+			var prevRow = arr[y-1] || [];
+			var nextRow = arr[y+1] || [];
+			// Run through the board and count the number of alive neighbors (1)
+			return [
+				prevRow[x-1], prevRow[x], prevRow[x+1],
+				arr[y][x-1], arr[y][x+1],
+				nextRow[x-1], nextRow[x], nextRow[x+1]
+			].reduce(function(prev, cur) {
+				// Total the neighbors.  If we have an undefined value (row/col does not
+				// exist), convert undefined to false using logical bool ops, and convert
+				// that to a number with unary op. Else add 1
+				return prev + +!!cur;
+			},0);
+		},
 		toString: function() {
 			//Debugging: Convert 2D board arr to string grid representation
 			return this.board.map(function(row) {
@@ -37,3 +63,4 @@ var Game = new Life([
 ]);
 
 console.log(Game.toString());
+Game.next();
